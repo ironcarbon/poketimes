7847a5cb08ce8a7553724fa281a2e5b9bcd368f9
ipek <ipekcivicakan@gmail.com>
2018-11-14 17:31:29 -0800

    React Router,setup and giving path

---
+++ src/Components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const about= () => {
+    return(
+        <div className="container">
+            <h4 className= "center">About</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit amet cum laboriosam totam id dicta tempore est distinctio delectus voluptates autem, eos ratione, dolorum necessitatibus eius, reprehenderit commodi enim voluptatem?</p>
+        </div>
+    )
+}
+
+export default about;
---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const home= () => {
+    return(
+        <div className="container">
+            <h4 className= "center">Home</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit amet cum laboriosam totam id dicta tempore est distinctio delectus voluptates autem, eos ratione, dolorum necessitatibus eius, reprehenderit commodi enim voluptatem?</p>
+        </div>
+    )
+}
+
+export default home;
