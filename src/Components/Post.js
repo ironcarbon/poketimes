094be5c68c6bc64dea60bbaa0b0be2c17bf58ec3
ipek <ipekcivicakan@gmail.com>
2018-11-15 11:20:49 -0800

    Continuing Route Parameters-2

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
 
 class Post extends Component{
     state={
-        id: null
+        post: null
     }
     componentDidMount(){
-        //console.log(this.props);
+        console.log(this.props);
         let currentId = this.props.match.params.post_id;
-        this.setState({id: currentId})
+        axios.get('https://jsonplaceholder.typicode.com/posts/' + currentId)
+        .then(res => {
+            this.setState({post: res.data})
+            //console.log(res);
+        })
     }
 
 render(){
+    const post = this.state.post;
+    const currentPost = post ? (
+        <div className= "post card" key={post.id}>
+                    <div className="card-content">
+                        <Link to={'/' + post.id}>
+                            <span className="card-title">{post.title}</span>
+                        </Link>
+                        <p>{post.body}</p>
+                    </div>
+                </div>
+    ) : (
+        <div className= "center">Loading post...</div>
+    )
+
     return(
         <div className="container">
-            <h4>{this.state.id} </h4>
+            <h4>{currentPost}</h4>
         </div>
     )
 }
